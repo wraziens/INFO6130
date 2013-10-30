59ab22d4cb2b50d9175d1e27b2d44809bf2a0696
Stacey Wrazien <wraziens@email.chop.edu>
2013-10-30 02:02:23 -0400

    Update DatabaseStore to handle Dates

--- DrinkingApp/src/com/example/drinkingapp/DatabaseStore.java
+++ DrinkingApp/src/com/example/drinkingapp/DatabaseStore.java
@@ -2,7 +2,9 @@ package com.example.drinkingapp;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 
@@ -14,6 +16,8 @@ public class DatabaseStore {
 	public Integer day;
 	public Integer month;
 	public Integer year;
+	public Integer hour;
+	public Integer minute;
 	public String time;
 	public String day_week;
 	//Should not be explicitly used but rather should use the below static methods
@@ -30,6 +34,8 @@ public class DatabaseStore {
 		SimpleDateFormat month_ft = new SimpleDateFormat("MM", Locale.US);
 		SimpleDateFormat year_ft = new SimpleDateFormat("yyyy", Locale.US);
 		SimpleDateFormat day_week_ft = new SimpleDateFormat("E", Locale.US);
+		SimpleDateFormat hour_ft = new SimpleDateFormat("HH", Locale.US);
+		SimpleDateFormat minute_ft = new SimpleDateFormat("mm",Locale.US);
 		SimpleDateFormat time_ft = new SimpleDateFormat("HH:mm:ss", Locale.US);
 		
 		this.day = Integer.parseInt(day_ft.format(date));
@@ -37,6 +43,32 @@ public class DatabaseStore {
 		this.year = Integer.parseInt(year_ft.format(date));
 		this.day_week = day_week_ft.format(date);
 		this.time = time_ft.format(date);
+		this.hour = Integer.parseInt(hour_ft.format(date));
+		this.minute=Integer.parseInt(minute_ft.format(date));
+	}
+	
+	public static ArrayList<DatabaseStore> sortByTime(List<DatabaseStore> store){
+		ArrayList<DatabaseStore> sorted = new ArrayList<DatabaseStore>();
+		
+		if (store.size() > 1) {
+			sorted.add(store.remove(0));
+			for (int i =0; i< store.size(); i++){
+				Date to_insert = store.get(i).date;
+				boolean inserted = false;
+				for (int j = 0; j< sorted.size(); j++){
+					if(to_insert.before(sorted.get(j).date)){
+						sorted.add(j, store.get(i));
+						break;
+					}
+				}
+				if (!inserted){
+					sorted.add(store.get(i));
+				}
+			}
+		}else if(store.size() == 0 ){
+			return (ArrayList<DatabaseStore>)store;
+		}
+		return sorted;
 	}
 	
 	public static DatabaseStore DatabaseSliderStore(String variable, String value, Date date){
