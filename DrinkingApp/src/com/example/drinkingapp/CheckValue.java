451cb6cc9bd12932cfe320f71fa48ef9236ea563
Stacey Wrazien <wraziens@email.chop.edu>
2013-11-15 18:56:43 -0500

    Add class to assist in handling many checkboxes

---
+++ DrinkingApp/src/com/example/drinkingapp/CheckValue.java
@@ -0,0 +1,13 @@
+package com.example.drinkingapp;
+
+import android.widget.CheckBox;
+
+public class CheckValue {
+	public String value;
+	public CheckBox view;
+	
+	public CheckValue(String str_value, CheckBox chk_view){
+		value = str_value;
+		view = chk_view;
+	}
+}
