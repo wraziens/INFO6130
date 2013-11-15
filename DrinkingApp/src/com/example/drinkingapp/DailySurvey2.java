14fa8f43da06361031888f0a05cfbef5d57f397d
Stacey Wrazien <wraziens@email.chop.edu>
2013-11-15 16:25:30 -0500

    Change wordle survey to checkboxes instead of textfields

--- DrinkingApp/src/com/example/drinkingapp/DailySurvey2.java
+++ DrinkingApp/src/com/example/drinkingapp/DailySurvey2.java
@@ -1,26 +1,62 @@
 package com.example.drinkingapp;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
 import android.view.WindowManager;
-import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.SeekBar;
-import android.widget.SeekBar.OnSeekBarChangeListener;
-import android.widget.TextView;
+import android.widget.CheckBox;
 
-public class DailySurvey2 extends Activity implements OnClickListener{
+public class DailySurvey2 extends Activity implements OnClickListener {
 
-	Button finish;
-	SeekBar happySeekBar;
-	TextView test;
-	EditText word1,word2,word3;
-	String seekbarResult,wordResult1,wordResult2,wordResult3;
-	Intent goToAssessment;
+	private Button finish;
+	
+	private ArrayList<CheckValue> checkboxes;
+	private ArrayList<String> selected_values;
+	private DatabaseHandler db;
+
+	private void addCheckboxValues(){
+		//add all the checkbox values
+		checkboxes.add(new CheckValue("happy", (CheckBox)findViewById(R.id.wordle_happy)));
+		checkboxes.add(new CheckValue("productive",(CheckBox)findViewById(R.id.wordle_productive)));
+		checkboxes.add(new CheckValue("lazy", (CheckBox)findViewById(R.id.wordle_lazy)));
+		checkboxes.add(new CheckValue("foggy",  (CheckBox)findViewById(R.id.wordle_foggy)));
+		checkboxes.add(new CheckValue("sick", (CheckBox)findViewById(R.id.wordle_sick)));
+		checkboxes.add(new CheckValue("strong", (CheckBox)findViewById(R.id.wordle_strong)));
+		checkboxes.add(new CheckValue("excited", (CheckBox)findViewById(R.id.wordle_excited)));
+		checkboxes.add(new CheckValue("angry", (CheckBox)findViewById(R.id.wordle_angry)));
+		checkboxes.add(new CheckValue("unhappy", (CheckBox)findViewById(R.id.wordle_unhappy)));
+		checkboxes.add(new CheckValue("sad", (CheckBox)findViewById(R.id.wordle_sad)));
+		checkboxes.add(new CheckValue("defeated", (CheckBox)findViewById(R.id.wordle_defeated)));
+		checkboxes.add(new CheckValue("social", (CheckBox)findViewById(R.id.wordle_social)));
+		checkboxes.add(new CheckValue("fun", (CheckBox)findViewById(R.id.wordle_fun)));
+		checkboxes.add(new CheckValue("sad", (CheckBox)findViewById(R.id.wordle_sad)));
+		checkboxes.add(new CheckValue("energetic", (CheckBox)findViewById(R.id.wordle_energetic)));
+		checkboxes.add(new CheckValue("healthy", (CheckBox)findViewById(R.id.wordle_healthy)));
+		checkboxes.add(new CheckValue("lonely", (CheckBox)findViewById(R.id.wordle_lonely)));
+		checkboxes.add(new CheckValue("successful", (CheckBox)findViewById(R.id.wordle_successful)));
+		checkboxes.add(new CheckValue("apathetic", (CheckBox)findViewById(R.id.wordle_apathetic)));
+		checkboxes.add(new CheckValue("optimistic", (CheckBox)findViewById(R.id.wordle_optimistic)));
+		checkboxes.add(new CheckValue("loved", (CheckBox)findViewById(R.id.wordle_loved)));
+		checkboxes.add(new CheckValue("embarrassed", (CheckBox)findViewById(R.id.wordle_embarrassed)));
+		checkboxes.add(new CheckValue("sloppy", (CheckBox)findViewById(R.id.wordle_sloppy)));
+		checkboxes.add(new CheckValue("out of control", (CheckBox)findViewById(R.id.wordle_out_control)));
+		checkboxes.add(new CheckValue("relaxed", (CheckBox)findViewById(R.id.wordle_relaxed)));
+		checkboxes.add(new CheckValue("uncomfortable", (CheckBox)findViewById(R.id.wordle_uncomfortable)));
+		checkboxes.add(new CheckValue("advernturous", (CheckBox)findViewById(R.id.wordle_adventurous)));
+		checkboxes.add(new CheckValue("stressed", (CheckBox)findViewById(R.id.wordle_stressed)));
+		checkboxes.add(new CheckValue("anxious", (CheckBox)findViewById(R.id.wordle_anxious)));
+		checkboxes.add(new CheckValue("depressed", (CheckBox)findViewById(R.id.wordle_depressed)));
+		checkboxes.add(new CheckValue("humorous", (CheckBox)findViewById(R.id.wordle_humorous)));
+		checkboxes.add(new CheckValue("regretful", (CheckBox)findViewById(R.id.wordle_regretful)));
+		checkboxes.add(new CheckValue("hopeful", (CheckBox)findViewById(R.id.wordle_hopeful)));
+		checkboxes.add(new CheckValue("outgoing", (CheckBox)findViewById(R.id.wordle_outgoing)));
+		checkboxes.add(new CheckValue("busy", (CheckBox)findViewById(R.id.wordle_busy)));
+	}
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -28,72 +64,53 @@ public class DailySurvey2 extends Activity implements OnClickListener{
 		requestWindowFeature(Window.FEATURE_NO_TITLE);// full screen
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
-		setContentView(R.layout.dailysurvey2);
-		//record=(Button)findViewById(R.id.bDS2Record);
-		word1=(EditText)findViewById(R.id.etDS2Word1);
-		word2=(EditText)findViewById(R.id.etDS2Word2);
-		word3=(EditText)findViewById(R.id.etDS2Word3);
-		finish=(Button)findViewById(R.id.bDS2Finish);
-		test=(TextView)findViewById(R.id.tvDS2Test);
-		happySeekBar=(SeekBar)findViewById(R.id.sbDS2);
-		//record.setOnClickListener(this);
+		setContentView(R.layout.wordlesurvey);
+		db = new DatabaseHandler(this);
+		
+		checkboxes = new ArrayList<CheckValue>();
+		addCheckboxValues();
+		
+		selected_values = new ArrayList<String>();
+		
+		finish = (Button) findViewById(R.id.wordle_finish);
 		finish.setOnClickListener(this);
-		happySeekBar.setProgress(0); //this is line 19
-		happySeekBar.setMax(100);
-		
-		initializeSeekBar();
 	}
 
-	private void initializeSeekBar() {
-		// TODO Auto-generated method stub
-		happySeekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener(){
-
-			@Override
-			public void onProgressChanged(SeekBar seekBar, int progress,
-					boolean fromUser) {
-				// TODO Auto-generated method stub
-				test.setText(String.valueOf(progress));
-				seekbarResult=String.valueOf(progress);
+	/*
+	 * Clears the current values in selected_values and 
+	 * Checks to see which of the checkboxes are checked. Adds
+	 * the corresponding string values to selected_values for the options
+	 * that are checked.
+	 */
+	private void checkCheckBoxes(){
+		selected_values.clear();
+		for (int i=0; i<checkboxes.size(); i++){
+			CheckValue c = checkboxes.get(i);
+			if(c.view.isChecked()){
+				selected_values.add(c.value);
 			}
-
-			@Override
-			public void onStartTrackingTouch(SeekBar seekBar) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			@Override
-			public void onStopTrackingTouch(SeekBar seekBar) {
-				// TODO Auto-generated method stub
-				
-			}
-		});
-		
-		
-	}
-
-	@Override
-	public void onClick(View arg0) {
-		// TODO Auto-generated method stub
-		switch(arg0.getId()){
-		/*
-		case R.id.bDS2Record:
-			break;
-			
-			*/
-		case R.id.bDS2Finish:
-			
-			wordResult1=word1.getText().toString();
-			wordResult2=word2.getText().toString();
-			wordResult3=word3.getText().toString();
-			finish();
-			break;
-			
 		}
-		
 	}
+	
+	private void saveToDb(){
+		db.clearAllValuesDay("wordle");
+		for(int i=0; i<selected_values.size(); i++){
+			db.addValue("wordle", selected_values.get(i));
+		}
+	}
+	
+	@Override
+	public void onClick(View view) {
+		switch (view.getId()){
+			case R.id.wordle_finish:
+				checkCheckBoxes();
+				saveToDb();
+				finish();
+				break;
+		}
+	}
+
 	protected void onPause() {
-		// TODO Auto-generated method stub
 		super.onPause();
 		finish();
 	}
