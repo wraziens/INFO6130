b2f79d2105d2d39b624b2fdcbfed78ed61b67e5c
alex pan <ayp8@cornell.edu>
2014-03-23 02:51:12 -0400

    working Boom Trickle Menu

--- DrinkingApp/src/cornell/eickleapp/MainMenu3.java
+++ DrinkingApp/src/cornell/eickleapp/MainMenu3.java
@@ -1,13 +1,16 @@
 package cornell.eickleapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
 import android.widget.Button;
 
-public class MainMenu3 extends Activity {
+public class MainMenu3 extends Activity implements OnClickListener {
+
+	Button drinkCounterMenu, surveyMenu, dataMenu;
 
 	FlyOutContainer root;
 
@@ -15,18 +18,50 @@ public class MainMenu3 extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
-	    getActionBar().hide();
-	    
-		this.root = (FlyOutContainer) this.getLayoutInflater().inflate(R.layout.menu3, null);
+		getActionBar().hide();
+
+		this.root = (FlyOutContainer) this.getLayoutInflater().inflate(
+				R.layout.menu3, null);
 
 		this.setContentView(root);
-		
-		
+
+		drinkCounterMenu = (Button) findViewById(R.id.bDrinkCounterMenu);
+		surveyMenu = (Button) findViewById(R.id.bSurveyMenu);
+		dataMenu = (Button) findViewById(R.id.bDataMenu);
+		drinkCounterMenu.setOnClickListener(this);
+		surveyMenu.setOnClickListener(this);
+		dataMenu.setOnClickListener(this);
+
 	}
 
-
-	public void toggleMenu(View v){
+	public void toggleMenu(View v) {
 		this.root.toggleMenu();
 	}
 
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		Intent goToThisPage;
+		switch(v.getId()){
+		
+		case R.id.bDrinkCounterMenu:
+			goToThisPage = new Intent(MainMenu3.this, DrinkCounter.class);
+			startActivity(goToThisPage);
+			
+			break;
+		case R.id.bSurveyMenu:
+			/*
+			goToThisPage = new Intent(MainMenu3.this, SurveyMenu.class);
+			startActivity(goToThisPage);
+			*/
+			break;
+		case R.id.bDataMenu:
+			/*
+			goToThisPage = new Intent(MainMenu3.this, DataMenu.class);
+			startActivity(goToThisPage);
+			*/
+		break;
+		}
+		
+	}
 }
