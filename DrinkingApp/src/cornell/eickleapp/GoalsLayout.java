5c2abdefdd1ee541f45d4d30e02252a99eaacdf3
alex pan <ayp8@cornell.edu>
2014-04-23 14:33:20 -0400

    Goals setup skeleton

---
+++ DrinkingApp/src/cornell/eickleapp/GoalsLayout.java
@@ -0,0 +1,15 @@
+package cornell.eickleapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GoalsLayout extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.goalslayout);
+	}
+
+}
